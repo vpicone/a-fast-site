1a1408d43b2eb41ed3ba3a83dadfe779b303909a
Matt Jared <matt.jared@vercel.com>
2025-06-24 22:04:02 -0400

    Michael Anderson

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,11 @@ import type React from "react"
 import type { Metadata } from "next"
 import "./globals.css"
 
-import { GeistMono } from "geist/font/mono"
+// import { GeistMono } from "geist/font/mono"
+// const font = GeistMono({
+//   subsets: ['latin'],
+//   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+// })
 
 export const metadata: Metadata = {
   title: "Dynamic City Explorer + ISS Tracker",
@@ -19,7 +23,13 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className={`${GeistMono.className} dark`}>
+    // <html lang="en" className={`${GeistMono.className} dark`}>
+    <html lang="en">
+      <head>
+        <link href="http://fonts.gstatic.com/s/roboto/v15/W5F8_SL0XFawnjxHGsZjJA.ttf" rel="stylesheet" />
+        <link href="http://fonts.gstatic.com/s/robotomono/v4/eJ4cxQe85Lo39t-LVoKa26CWcynf_cDxXwCLxiixG1c.ttf" rel="stylesheet" />
+        <link href="http://fonts.gstatic.com/s/rochester/v6/bnj8tmQBiOkdji_G_yvypg.ttf" rel="stylesheet" />
+      </head>
       <body className=" bg-black text-white min-h-screen">
         <Header />
         {children}
